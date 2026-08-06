83f8a115ed49b0bc8d473ef146e55b3f4d387db0
alvv <alvv2115@gmail.com>
2026-08-06 18:15:43 +0200

    feat: added hero section

---
+++ src/app/components/Hero/Hero.tsx
@@ -0,0 +1,22 @@
+import CopyIp from "./CopyIp";
+
+export default function Hero() {
+  return (
+    <main className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden text-text-secondary">
+      <div className="absolute inset-0 -z-10 bg-[url('/bg-body.png')] bg-cover bg-center bg-no-repeat blur-xs scale-105" />
+
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-fluid-hero font-bold text-inherit ">
+          EnterCraft.pl
+        </h1>
+        <p className="text-fluid-subtitle font-medium ">
+          Twój ulubiony serwer minecraft!
+        </p>
+      </div>
+
+      <div className="mt-6">
+        <CopyIp />
+      </div>
+    </main>
+  );
+}
